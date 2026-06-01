f3d1bcdc877ccad76db6239d35304fd0f0078f59
Gabriel P. <gabrielpereiraemdb@gmail.com>
2026-06-01 00:05:45 -0300

    Mais atividade feitas

--- Estrutura Condicional/atividade 7/script.js
+++ Estrutura Condicional/atividade 7/script.js
@@ -1,2 +1,10 @@
 let number = Number(prompt("Type a number"))
 
+if (number % 2 == 0){
+    alert("The number is pair number")
+}
+
+else{
+    alert("The number is odd number")
+}
+
---
+++ Estrutura Condicional/atividade 8/script.js
@@ -0,0 +1,9 @@
+let score = Number(prompt("What was score?"))
+
+if (score >= 7){
+    alert("You are approved")
+}
+
+else{
+    alert("You are reproved")
+}
